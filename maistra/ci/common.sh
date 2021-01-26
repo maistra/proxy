d1fdf4bbfa3540723a4cff03d084b55917b47c71
Jonh Wendell <jonh.wendell@redhat.com>
2021-01-26 15:00:35 -0500

    Build WASM extensions

--- maistra/ci/common.sh
+++ maistra/ci/common.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 OUTPUT_TO_IGNORE="\
 linux-sandbox-pid1.cc|\
 process-tools.cc|\
@@ -6,3 +8,35 @@ rules_foreign_cc: Cleaning temp directories|\
 proto but not used|\
 INFO: From\
 "
+
+ARCH=$(uname -p)
+if [ "${ARCH}" = "ppc64le" ]; then
+  ARCH="ppc"
+fi
+export ARCH
+
+COMMON_FLAGS="\
+    --incompatible_linkopts_to_linklibs \
+    --config=release \
+    --config=${ARCH} \
+    --local_ram_resources=12288 \
+    --local_cpu_resources=4 \
+    --jobs=4 \
+    --disk_cache=/bazel-cache \
+"
+
+function bazel_build() {
+  bazel build \
+    ${COMMON_FLAGS} \
+    "${@}" \
+  2>&1 | grep -v -E "${OUTPUT_TO_IGNORE}"
+}
+
+function bazel_test() {
+  bazel test \
+    ${COMMON_FLAGS} \
+    --build_tests_only \
+    --test_env=ENVOY_IP_TEST_VERSIONS=v4only \
+    "${@}" \
+  2>&1 | grep -v -E "${OUTPUT_TO_IGNORE}"
+}
