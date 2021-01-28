d8f1124bf29552d138efbf9e37920f3c29a8c85f
Jonh Wendell <jonh.wendell@redhat.com>
2021-01-28 16:29:10 -0500

    Build WASM extensions (#91)

--- maistra/ci/pre-submit.sh
+++ maistra/ci/pre-submit.sh
@@ -4,48 +4,35 @@ set -e
 set -o pipefail
 set -x
 
+# shellcheck disable=SC1091
 source /opt/rh/gcc-toolset-9/enable
 
-DIR=$(cd $(dirname $0) ; pwd -P)
-source "${DIR}/common.sh"
+DIR=$(cd "$(dirname "$0")" ; pwd -P)
 
-ARCH=$(uname -p)
-if [ "${ARCH}" = "ppc64le" ]; then
-  ARCH="ppc"
-fi
-export ARCH
+# shellcheck disable=SC1090
+source "${DIR}/common.sh"
 
 # Fix path to the vendor deps
 sed -i "s|=/work/|=$(pwd)/|" maistra/bazelrc-vendor
 
-# Build
-bazel build \
-  --incompatible_linkopts_to_linklibs \
-  --config=release \
-  --config=${ARCH} \
-  --local_ram_resources=12288 \
-  --local_cpu_resources=4 \
-  --jobs=4 \
-  --disk_cache=/bazel-cache \
-  //src/envoy:envoy \
-  2>&1 | grep -v -E "${OUTPUT_TO_IGNORE}"
+# Build WASM extensions first
+bazel_build //extensions:stats.wasm
+bazel_build //extensions:metadata_exchange.wasm
+bazel_build //extensions:attributegen.wasm
+bazel_build @envoy//test/tools/wee8_compile:wee8_compile_tool
+
+bazel-bin/external/envoy/test/tools/wee8_compile/wee8_compile_tool bazel-bin/extensions/stats.wasm bazel-bin/extensions/stats.compiled.wasm
+bazel-bin/external/envoy/test/tools/wee8_compile/wee8_compile_tool bazel-bin/extensions/metadata_exchange.wasm bazel-bin/extensions/metadata_exchange.compiled.wasm
+bazel-bin/external/envoy/test/tools/wee8_compile/wee8_compile_tool bazel-bin/extensions/attributegen.wasm bazel-bin/extensions/attributegen.compiled.wasm
+
+echo "WASM extensions built succesfully. Now building envoy binary."
+
+# Build Envoy
+bazel_build //src/envoy:envoy
 
 echo "Build succeeded. Binary generated:"
 bazel-bin/src/envoy/envoy --version
 
 # Run tests
-bazel test \
-  --incompatible_linkopts_to_linklibs \
-  --config=release \
-  --config=${ARCH} \
-  --local_ram_resources=12288 \
-  --local_cpu_resources=4 \
-  --jobs=4 \
-  --test_output=all \
-  --build_tests_only \
-  --test_env=ENVOY_IP_TEST_VERSIONS=v4only \
-  --disk_cache=/bazel-cache \
-  -- //src/... //test/...  \
-  2>&1 | grep -v -E "${OUTPUT_TO_IGNORE}"
-
-env ENVOY_PATH=bazel-bin/src/envoy/envoy GO111MODULE=on go test ./...
+bazel_test //src/... //test/...
+env ENVOY_PATH=bazel-bin/src/envoy/envoy GO111MODULE=on WASM=true go test ./...
