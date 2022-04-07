8cc5bcee162f6115bf039c0996e337308bcf9b61
Jonh Wendell <jonh.wendell@redhat.com>
2022-04-07 10:11:27 -0400

    Tweaks to the build system to support clang

--- maistra/ci/pre-submit.sh
+++ maistra/ci/pre-submit.sh
@@ -4,8 +4,7 @@ set -e
 set -o pipefail
 set -x
 
-# shellcheck disable=SC1091
-source /opt/rh/gcc-toolset-9/enable
+export CC=clang CXX=clang++
 
 DIR=$(cd "$(dirname "$0")" ; pwd -P)
 
@@ -13,29 +12,30 @@ DIR=$(cd "$(dirname "$0")" ; pwd -P)
 source "${DIR}/common.sh"
 
 # Build WASM extensions first
-CC=clang CXX=clang++ bazel_build //extensions:stats.wasm
-CC=clang CXX=clang++ bazel_build //extensions:metadata_exchange.wasm
-CC=clang CXX=clang++ bazel_build //extensions:attributegen.wasm
-CC=cc CXX=g++ bazel_build @envoy//test/tools/wee8_compile:wee8_compile_tool
+time bazel_build //extensions:stats.wasm
+time bazel_build //extensions:metadata_exchange.wasm
+time bazel_build //extensions:attributegen.wasm
+time bazel_build @envoy//test/tools/wee8_compile:wee8_compile_tool
 
-CC=clang CXX=clang++ bazel-bin/external/envoy/test/tools/wee8_compile/wee8_compile_tool bazel-bin/extensions/stats.wasm bazel-bin/extensions/stats.compiled.wasm
-CC=clang CXX=clang++ bazel-bin/external/envoy/test/tools/wee8_compile/wee8_compile_tool bazel-bin/extensions/metadata_exchange.wasm bazel-bin/extensions/metadata_exchange.compiled.wasm
-CC=clang CXX=clang++ bazel-bin/external/envoy/test/tools/wee8_compile/wee8_compile_tool bazel-bin/extensions/attributegen.wasm bazel-bin/extensions/attributegen.compiled.wasm
+bazel-bin/external/envoy/test/tools/wee8_compile/wee8_compile_tool bazel-bin/extensions/stats.wasm bazel-bin/extensions/stats.compiled.wasm
+bazel-bin/external/envoy/test/tools/wee8_compile/wee8_compile_tool bazel-bin/extensions/metadata_exchange.wasm bazel-bin/extensions/metadata_exchange.compiled.wasm
+bazel-bin/external/envoy/test/tools/wee8_compile/wee8_compile_tool bazel-bin/extensions/attributegen.wasm bazel-bin/extensions/attributegen.compiled.wasm
 
 echo "WASM extensions built succesfully. Now building envoy binary."
 
 # Build Envoy
-CC=cc CXX=g++ bazel_build //src/envoy:envoy
+time bazel_build //src/envoy:envoy
 
 echo "Build succeeded. Binary generated:"
 bazel-bin/src/envoy/envoy --version
 
 # Run tests
-CC=cc CXX=g++ bazel_test //src/... //test/...
+time bazel_test //src/... //test/...
 
 export ENVOY_PATH=bazel-bin/src/envoy/envoy
 export GO111MODULE=on
 export GOPATH=$HOME/go
 
-go test ./...
-WASM=true go test ./test/envoye2e/stats_plugin/...
+time go test ./...
+export WASM=true
+time go test ./test/envoye2e/stats_plugin/...
