d42485205b308de7763cb08c43b8732e4dbec49d
Jonh Wendell <jonh.wendell@redhat.com>
2022-08-25 17:20:23 -0400

    Preparation to build, before vendoring

--- maistra/ci/pre-submit.sh
+++ maistra/ci/pre-submit.sh
@@ -10,16 +10,16 @@ DIR=$(cd "$(dirname "$0")" ; pwd -P)
 source "${DIR}/common.sh"
 
 # Build WASM extensions first
-time bazel_build //extensions:stats.wasm
-time bazel_build //extensions:metadata_exchange.wasm
-time bazel_build //extensions:attributegen.wasm
-time bazel_build @envoy//test/tools/wee8_compile:wee8_compile_tool
+# time bazel_build //extensions:stats.wasm
+# time bazel_build //extensions:metadata_exchange.wasm
+# time bazel_build //extensions:attributegen.wasm
+# time bazel_build @envoy//test/tools/wee8_compile:wee8_compile_tool
 
-bazel-bin/external/envoy/test/tools/wee8_compile/wee8_compile_tool bazel-bin/extensions/stats.wasm bazel-bin/extensions/stats.compiled.wasm
-bazel-bin/external/envoy/test/tools/wee8_compile/wee8_compile_tool bazel-bin/extensions/metadata_exchange.wasm bazel-bin/extensions/metadata_exchange.compiled.wasm
-bazel-bin/external/envoy/test/tools/wee8_compile/wee8_compile_tool bazel-bin/extensions/attributegen.wasm bazel-bin/extensions/attributegen.compiled.wasm
+# bazel-bin/external/envoy/test/tools/wee8_compile/wee8_compile_tool bazel-bin/extensions/stats.wasm bazel-bin/extensions/stats.compiled.wasm
+# bazel-bin/external/envoy/test/tools/wee8_compile/wee8_compile_tool bazel-bin/extensions/metadata_exchange.wasm bazel-bin/extensions/metadata_exchange.compiled.wasm
+# bazel-bin/external/envoy/test/tools/wee8_compile/wee8_compile_tool bazel-bin/extensions/attributegen.wasm bazel-bin/extensions/attributegen.compiled.wasm
 
-echo "WASM extensions built succesfully. Now building envoy binary."
+# echo "WASM extensions built succesfully. Now building envoy binary."
 
 # Build Envoy
 time bazel_build //src/envoy:envoy
@@ -35,5 +35,5 @@ export GO111MODULE=on
 export GOPATH=$HOME/go
 
 time go test ./...
-export WASM=true
-time go test ./test/envoye2e/stats_plugin/...
+# export WASM=true
+# time go test ./test/envoye2e/stats_plugin/...
